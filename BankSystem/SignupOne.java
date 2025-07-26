51e45f00909dc6936ec966e99d00fc1885d538e9
Pramod Thorat <pramodthorat410@gmail.com>
2025-07-26 20:46:13 +0530

    gender male text field

--- BankSystem/SignupOne.java
+++ BankSystem/SignupOne.java
@@ -75,12 +75,12 @@ public class SignupOne extends JFrame implements ActionListener {
         gender.setFont(new Font("Raleway", Font.BOLD, 20));
         gender.setBounds(100, 300, 150, 30);
         add(gender);
-/*
+
         male = new JRadioButton("Male");
         male.setBounds(300, 300, 90, 30);
         male.setBackground(Color.WHITE);
         add(male);
-
+/*
         female = new JRadioButton("Female");
         female.setBounds(450, 300, 90, 30);
         female.setBackground(Color.WHITE);
